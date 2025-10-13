48cd8ed13ec52f9548386d9fac9fc398df69a410
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2025-10-13 16:17:09 +0100

    basic nix flake for development

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "obol-stack -- A Kubernetes-based Ethereum stack for running decentralised applications. ";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devshell.flakeModule];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        devshells.default = {
+          packages = with pkgs; [
+            # bash
+            shellcheck
+
+            # golang
+            go
+            go-tools
+            gopls
+            gotools
+
+            # nix
+            alejandra
+          ];
+        };
+      };
+    };
+}
